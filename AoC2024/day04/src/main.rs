48211c5f32e3949e303400d0f0691c06be81e283
sprunq <52038911+sprunq@users.noreply.github.com>
2024-12-04 10:00:25 +0100

    clippy and opt

--- AoC2024/day04/src/main.rs
+++ AoC2024/day04/src/main.rs
@@ -1,53 +1,36 @@
+use std::time::Instant;
+
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    let now = std::time::Instant::now();
-    let p1 = part1();
-    println!("Part 1: {} [{:?}]", p1, now.elapsed());
+    let now = Instant::now();
+    let input: Vec<Vec<u8>> = INPUT.lines().map(|line| line.bytes().collect()).collect();
+    let p1 = part1(&input);
+    let p2 = part2(&input);
+    let elapsed = now.elapsed();
 
-    let now = std::time::Instant::now();
-    let p2 = part2();
-    println!("Part 2: {} [{:?}]", p2, now.elapsed());
+    println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
+    println!("Elapsed: {:?}", elapsed);
 }
 
-fn part2() -> u32 {
-    let needle = [b'M', b'A', b'S'];
-    let needle_rev = [b'S', b'A', b'M'];
-
-    let input_array: Vec<Vec<u8>> = INPUT.lines().map(|line| line.bytes().collect()).collect();
-
-    let mut count = 0;
-    for h in 1..input_array.len() - 1 {
-        for w in 1..input_array[h].len() - 1 {
-            let tl_to_br = range_diagonal_tl_to_br(&input_array, h - 1, w - 1);
-            let tr_to_bl = range_diagonal_tr_to_bl(&input_array, h - 1, w + 1);
-
-            let is_lr_match = matches(&tl_to_br, &needle) || matches(&tl_to_br, &needle_rev);
-            let is_rl_match = matches(&tr_to_bl, &needle) || matches(&tr_to_bl, &needle_rev);
-
-            if is_lr_match && is_rl_match {
-                count += 1;
-            }
-        }
-    }
-
-    count
-}
-
-fn part1() -> u32 {
+fn part1(input: &[Vec<u8>]) -> u32 {
     let needle = [b'X', b'M', b'A', b'S'];
     let needle_rev = [b'S', b'A', b'M', b'X'];
 
-    let input: Vec<Vec<u8>> = INPUT.lines().map(|line| line.bytes().collect()).collect();
-
     let mut count = 0;
 
-    for x in 0..input.len() {
-        for y in 0..input[x].len() {
-            let l_to_r = range_left_to_right(&input, x, y);
-            let t_to_b = range_top_to_bottom(&input, x, y);
-            let tl_to_br = range_diagonal_tl_to_br(&input, x, y);
-            let tr_to_bl = range_diagonal_tr_to_bl(&input, x, y);
+    for h in 0..input.len() {
+        for w in 0..input[h].len() {
+            let current = input[h][w];
+            if !(current == b'X' || current == b'S') {
+                continue;
+            }
+
+            let l_to_r = range_left_to_right(input, h, w);
+            let t_to_b = range_top_to_bottom(input, h, w);
+            let tl_to_br = range_diagonal_tl_to_br(input, h, w);
+            let tr_to_bl = range_diagonal_tr_to_bl(input, h, w);
 
             let match_l_to_r = matches(&l_to_r, &needle) || matches(&l_to_r, &needle_rev);
             let match_t_to_b = matches(&t_to_b, &needle) || matches(&t_to_b, &needle_rev);
@@ -64,54 +47,84 @@ fn part1() -> u32 {
     count
 }
 
+fn part2(input: &[Vec<u8>]) -> u32 {
+    let needle = [b'M', b'A', b'S'];
+    let needle_rev = [b'S', b'A', b'M'];
+
+    let mut count = 0;
+    for h in 1..input.len() - 1 {
+        for w in 1..input[h].len() - 1 {
+            if input[h][w] != b'A' {
+                continue;
+            }
+
+            let tl_to_br = range_diagonal_tl_to_br(input, h - 1, w - 1);
+            let tr_to_bl = range_diagonal_tr_to_bl(input, h - 1, w + 1);
+
+            let is_lr_match = matches(&tl_to_br, &needle) || matches(&tl_to_br, &needle_rev);
+            let is_rl_match = matches(&tr_to_bl, &needle) || matches(&tr_to_bl, &needle_rev);
+
+            if is_lr_match && is_rl_match {
+                count += 1;
+            }
+        }
+    }
+
+    count
+}
+
 fn matches<const N: usize>(haystack: &Option<[u8; N]>, needle: &[u8; N]) -> bool {
     haystack.map_or(false, |haystack| haystack == *needle)
 }
 
 fn range_left_to_right<const N: usize>(
-    input_array: &Vec<Vec<u8>>,
+    input_array: &[Vec<u8>],
     x: usize,
     y: usize,
 ) -> Option<[u8; N]> {
     let mut output = [0; N];
-    for i in 0..N {
-        output[i] = *input_array.get(x)?.get(y + i)?;
+    for (idx, val) in output.iter_mut().enumerate() {
+        *val = *input_array.get(x)?.get(y + idx)?;
     }
     Some(output)
 }
 
 fn range_top_to_bottom<const N: usize>(
-    input_array: &Vec<Vec<u8>>,
+    input_array: &[Vec<u8>],
     x: usize,
     y: usize,
 ) -> Option<[u8; N]> {
     let mut output = [0; N];
-    for i in 0..N {
-        output[i] = *input_array.get(x + i)?.get(y)?;
+    for (idx, val) in output.iter_mut().enumerate() {
+        *val = *input_array.get(x + idx)?.get(y)?;
     }
     Some(output)
 }
 
 fn range_diagonal_tl_to_br<const N: usize>(
-    input_array: &Vec<Vec<u8>>,
+    input_array: &[Vec<u8>],
     x: usize,
     y: usize,
 ) -> Option<[u8; N]> {
     let mut output = [0; N];
-    for i in 0..N {
-        output[i] = *input_array.get(x + i)?.get(y + i)?;
+    for (idx, val) in output.iter_mut().enumerate() {
+        *val = *input_array.get(x + idx)?.get(y + idx)?;
     }
     Some(output)
 }
 
 fn range_diagonal_tr_to_bl<const N: usize>(
-    input_array: &Vec<Vec<u8>>,
+    input_array: &[Vec<u8>],
     x: usize,
     y: usize,
 ) -> Option<[u8; N]> {
+    if y < N - 1 {
+        return None;
+    }
+
     let mut output = [0; N];
-    for i in 0..N {
-        output[i] = *input_array.get(x + i)?.get(y - i)?;
+    for (idx, val) in output.iter_mut().enumerate() {
+        *val = *input_array.get(x + idx)?.get(y - idx)?;
     }
     Some(output)
 }
