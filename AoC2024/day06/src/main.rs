68d4d010b68cc89b7ede8849549fd2a6a5aeea49
sprunq <52038911+sprunq@users.noreply.github.com>
2024-12-07 10:14:12 +0100

    day06

--- AoC2024/day06/src/main.rs
+++ AoC2024/day06/src/main.rs
@@ -1,89 +1,15 @@
-use std::collections::HashSet;
-
+use ahash::AHashSet;
 use bitvec::vec::BitVec;
+use itertools::Itertools;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 const INPUT: &str = include_str!("input.txt");
 
-fn main() {
-    let (grid, start) = parse_input(INPUT);
-    let part1 = part1(&grid, start);
-    println!("Part 1: {}", part1);
-    let part2 = part2(&grid, start);
-    println!("Part 2: {}", part2);
-}
+const OBSTACLE: bool = true;
+const EMPTY: bool = false;
 
-fn part1(input: &[BitVec], start: (usize, usize)) -> usize {
-    let visited = walk(start.0, start.1, Direction::Up, input);
-    visited
-        .unwrap()
-        .iter()
-        .map(|(x, y, _)| (*x, *y))
-        .collect::<HashSet<(usize, usize)>>()
-        .len()
-}
-
-fn part2(input: &[BitVec], start: (usize, usize)) -> usize {
-    let mut timeloops = 0;
-    let visited = walk(start.0, start.1, Direction::Up, input).unwrap();
-
-    for (x, y, direction) in visited.into_iter() {
-        let Some((look_at_h, look_at_w)) = direction.mov(x, y) else {
-            continue;
-        };
-
-        if look_at_h >= input.len() || look_at_w >= input[0].len() {
-            continue;
-        }
-
-        let mut new_grid = input.to_vec();
-        new_grid[look_at_h].set(look_at_w, true);
-
-        if walk(start.0, start.1, Direction::Up, &new_grid).is_err() {
-            timeloops += 1;
-        }
-    }
-
-    timeloops
-}
-
-fn walk(
-    start_x: usize,
-    start_: usize,
-    start_dir: Direction,
-    input: &[BitVec],
-) -> Result<HashSet<(usize, usize, Direction)>, ()> {
-    let mut direction = start_dir;
-    let mut current_pos = (start_x, start_);
-
-    let mut visited = HashSet::new();
-    visited.insert((start_x, start_, start_dir));
-
-    loop {
-        let Some((look_at_h, look_at_w)) = direction.mov(current_pos.0, current_pos.1) else {
-            break;
-        };
-
-        if look_at_h >= input.len() || look_at_w >= input[0].len() {
-            // guard is out of bounds
-            break;
-        }
-
-        let looking_at = *input[look_at_h].get(look_at_w).unwrap();
-        if looking_at {
-            direction = direction.next();
-        } else {
-            current_pos = (look_at_h, look_at_w);
-        }
-
-        if visited.contains(&(current_pos.0, current_pos.1, direction)) {
-            // guard is stuck in a loop
-            return Err(());
-        }
-
-        visited.insert((current_pos.0, current_pos.1, direction));
-    }
-    Ok(visited)
-}
+#[derive(Debug)]
+struct TimeLoopError;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Direction {
@@ -105,24 +31,99 @@ impl Direction {
 
     pub fn mov(&self, x: usize, y: usize) -> Option<(usize, usize)> {
         match self {
-            Direction::Up => {
-                if x == 0 {
-                    None
-                } else {
-                    Some((x - 1, y))
-                }
-            }
+            Direction::Up if x > 0 => Some((x - 1, y)),
             Direction::Right => Some((x, y + 1)),
             Direction::Down => Some((x + 1, y)),
-            Direction::Left => {
-                if y == 0 {
-                    None
-                } else {
-                    Some((x, y - 1))
-                }
-            }
+            Direction::Left if y > 0 => Some((x, y - 1)),
+            _ => None,
         }
     }
+}
+
+fn main() {
+    let t0 = std::time::Instant::now();
+    let (grid, start) = parse_input(INPUT);
+    let part1 = part1(&grid, start);
+    let part2 = part2(&grid, start);
+    println!("Time: {:?}", t0.elapsed());
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
+}
+
+fn part1(input: &[BitVec], start: (usize, usize)) -> usize {
+    walk(start.0, start.1, Direction::Up, input)
+        .expect("Time loop in initial walk")
+        .iter()
+        .map(|(x, y, _)| (*x, *y))
+        .unique()
+        .count()
+}
+
+fn part2(input: &[BitVec], start: (usize, usize)) -> usize {
+    let guard_positions =
+        walk(start.0, start.1, Direction::Up, input).expect("Time loop in initial walk");
+
+    let possible_obstacle_positions = guard_positions
+        .iter()
+        .map(|(x, y, _)| (*x, *y))
+        .filter(|(x, y)| *x != start.0 || *y != start.1)
+        .unique()
+        .collect_vec();
+
+    possible_obstacle_positions
+        .into_par_iter()
+        .filter(|(x, y)| {
+            let mut new_grid = input.to_owned();
+            new_grid[*x].set(*y, true);
+            walk(start.0, start.1, Direction::Up, &new_grid).is_err()
+        })
+        .count()
+}
+
+fn walk(
+    start_x: usize,
+    start_: usize,
+    start_dir: Direction,
+    grid: &[BitVec],
+) -> Result<AHashSet<(usize, usize, Direction)>, TimeLoopError> {
+    let mut dir = start_dir;
+    let mut pos = (start_x, start_);
+
+    let mut visited = AHashSet::new();
+    visited.insert((start_x, start_, start_dir));
+
+    loop {
+        // out of bounds
+        let Some((look_at_h, look_at_w)) = dir.mov(pos.0, pos.1) else {
+            break;
+        };
+        if look_at_h >= grid.len() || look_at_w >= grid[0].len() {
+            break;
+        }
+
+        (pos, dir) = step(grid, look_at_h, look_at_w, dir, pos);
+
+        if visited.contains(&(pos.0, pos.1, dir)) {
+            return Err(TimeLoopError);
+        }
+
+        visited.insert((pos.0, pos.1, dir));
+    }
+    Ok(visited)
+}
+
+fn step(
+    grid: &[BitVec],
+    look_at_h: usize,
+    look_at_w: usize,
+    mut dir: Direction,
+    mut pos: (usize, usize),
+) -> ((usize, usize), Direction) {
+    match grid[look_at_h][look_at_w] {
+        OBSTACLE => dir = dir.next(),
+        EMPTY => pos = (look_at_h, look_at_w),
+    }
+    (pos, dir)
 }
 
 fn parse_input(input: &str) -> (Vec<BitVec>, (usize, usize)) {
@@ -133,11 +134,11 @@ fn parse_input(input: &str) -> (Vec<BitVec>, (usize, usize)) {
             .chars()
             .enumerate()
             .map(|(idx, c)| match c {
-                '.' => false,
-                '#' => true,
+                '.' => EMPTY,
+                '#' => OBSTACLE,
                 '^' => {
                     guard_pos = Some((grid.len(), idx));
-                    false
+                    EMPTY
                 }
                 _ => unreachable!(),
             })
